4102de992f30adfff09843213b12ff472e848312
Nayara Souza <nsouza@thoughtworks.com>
2018-02-03 21:54:20 -0200

    [#2] @nayararibeiro - Refactor to fix a11y warnings and remove code duplication on header and footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,11 +7,12 @@ class Header extends Component {
         return (
             <div className='header'>
                 <div className='page_logo'>
-                    <a href="/" />
                     <div className='logo_header'>
-                        <img src={bookworm_logo} alt="Page logo - draw of black bookshelf"/>
+                        <a href="/">
+                            <img src={bookworm_logo} alt="Page logo - draw of black bookshelf"/>
+                            <span className='page_title' title='BOOKWORM'>BOOKWORM</span>
+                        </a>
                     </div>
-                    <div className='page_title' title='BOOKWORM'>BOOKWORM</div>
                 </div>
             </div>
 
