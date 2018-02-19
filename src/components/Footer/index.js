d306d728a86ab29712b32c5325d17f773781d912
Nayara Souza <nsouza@thoughtworks.com>
2018-02-18 21:24:56 -0300

    [REFACTOR] - Change footer to be a const

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import github_logo from '../../github-logo.svg';
 
 import './Footer.css'
 
-class Footer extends Component {
-    render() {
-        return (
-            <div className='footer'>
-                <div className='footer_title'>WHERE THE LIBRARY LIVES</div>
-                <div className='git_logo'>
-                    <a href="https://github.com/NayaraRibeiro/bookworm" title='Github library page'> <img src={github_logo} alt="Github logo" /></a>
-                </div>
-            </div>
-        );
-    }
-}
+const Footer = () => (
+    <div className='footer'>
+        <div className='footer_title'>WHERE THE LIBRARY LIVES</div>
+        <div className='git_logo'>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/NayaraRibeiro/bookworm" title='Github library page'>
+                <img src={github_logo} alt="Github logo" />
+            </a>
+        </div>
+    </div>
+);
 
 export default Footer;
